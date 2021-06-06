e1faada9a8cad6f492201130e5a4720a9f1f171a
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2021-06-06 20:37:22 +0300

    non UserUpdate

--- app/src/main/java/com/pyliavskyi/apz_android/UserProfileComponentActivity.java
+++ app/src/main/java/com/pyliavskyi/apz_android/UserProfileComponentActivity.java
@@ -34,6 +34,7 @@ public class UserProfileComponentActivity extends Activity {
     private Button confirmEditingButton;
 
     SessionManager session;
+    HashMap<String, String> userinfo;
 
     private class GetUserInfoRequest extends AsyncTask<URL, Void, String> {
 
@@ -121,102 +122,6 @@ public class UserProfileComponentActivity extends Activity {
         return response;
     }
 
-    private class UpdateUserInfoRequest extends AsyncTask<URL, Void, String> {
-
-        @Override
-        protected String doInBackground(URL... urls) {
-            String response = null;
-            try {
-                response = updateUserInfoResponse(urls[0]);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return response;
-        }
-
-        @Override
-        protected void onPostExecute(String response) {
-            if (response != null) {
-                JSONObject json = null;
-                try {
-                    json = new JSONObject(response);
-                    Toast.makeText(getApplicationContext(), json.toString(), Toast.LENGTH_SHORT)
-                            .show();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                Intent i = new Intent(getApplicationContext(), SignInActivity.class);
-                startActivity(i);
-                finish();
-            } else {
-
-                Toast.makeText(getApplicationContext(), "User unknown", Toast.LENGTH_SHORT)
-                        .show();
-            }
-        }
-    }
-
-    public String updateUserInfoResponse(URL url) throws IOException {
-        session = new SessionManager(getApplicationContext());
-        HashMap<String, String> userinfo = session.getUserDetails();
-        JSONObject jsonObject = null;
-        try {
-            jsonObject = new JSONObject(
-                    "{\"id\":" + Integer.parseInt(userId.getText().toString())  +
-                            ",\"name\": " + userName.getText().toString() +
-                            ",\"surname\": " + userSurname.getText().toString() +
-                            ",\"feature\": " + userFeature.getText().toString() + "}");
-        } catch (JSONException err) {
-            Toast.makeText(getApplicationContext(), err.toString(), Toast.LENGTH_SHORT).show();
-
-        }
-
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("PUT");
-        connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-        connection.setRequestProperty("token", userinfo.get("token"));
-        connection.setRequestProperty("Accept", "application/json");
-        connection.setDoOutput(true);
-        connection.setDoInput(true);
-
-        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
-        wr.writeBytes(jsonObject.toString());
-        wr.flush();
-        wr.close();
-
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(
-                        connection.getInputStream()));
-        String response = "";
-        String inputLine;
-
-        while ((inputLine = in.readLine()) != null)
-            response += inputLine;
-        in.close();
-        return response;
-    }
-
-    private boolean isValid() {
-
-        boolean validUserId = (userId.getText().toString().length() > 1);
-        boolean validUserName = (userName.getText().toString().length() > 1);
-        boolean validUserSurname = (userSurname.getText().toString().length() > 1);
-        boolean validUserFeature = (userFeature.getText().toString().length() > 1);
-
-
-        Toast.makeText(getApplicationContext(), validUserId ? "1true" : "1false", Toast.LENGTH_SHORT).show();
-        Toast.makeText(getApplicationContext(), validUserName ? "2true" : "2false", Toast.LENGTH_SHORT).show();
-        Toast.makeText(getApplicationContext(), validUserSurname ? "2true" : "2false", Toast.LENGTH_SHORT).show();
-        Toast.makeText(getApplicationContext(), validUserFeature ? "2true" : "2false", Toast.LENGTH_SHORT).show();
-
-        if (validUserId && validUserName && validUserSurname && validUserFeature) {
-            return true;
-        }
-        return false;
-    }
-
     private void GetUserInfoRun() {
         Resources res = getResources();
         String base = res.getString(R.string.baseUrl);
@@ -231,35 +136,11 @@ public class UserProfileComponentActivity extends Activity {
         new GetUserInfoRequest().execute(url);
     }
 
-    private void UpdateUserInfoRun() {
-
-        confirmEditingButton = findViewById(R.id.button2);
-
-        confirmEditingButton.setOnClickListener(v -> {
-
-            Resources res = getResources();
-            String base = res.getString(R.string.baseUrl);
-            String url1 = base + "/User/UpdateProfile";
-            URL url = null;
-            try {
-                url = new URL(url1);
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
-
-            new UpdateUserInfoRequest().execute(url);
-        });
-
-    }
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile_component);
 
         GetUserInfoRun();
-        UpdateUserInfoRun();
-
     }
 }
