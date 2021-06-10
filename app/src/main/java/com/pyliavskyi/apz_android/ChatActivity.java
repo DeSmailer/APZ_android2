4b802b21141cdcb3090b95c66cac5b5b852c5416
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2021-06-10 21:52:10 +0300

    da syka

--- app/src/main/java/com/pyliavskyi/apz_android/ChatActivity.java
+++ app/src/main/java/com/pyliavskyi/apz_android/ChatActivity.java
@@ -1,7 +1,23 @@
 package com.pyliavskyi.apz_android;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.gson.JsonElement;
+import com.microsoft.signalr.HubConnectionBuilder;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
 
 import microsoft.aspnet.signalr.client.Action;
 import microsoft.aspnet.signalr.client.ErrorCallback;
@@ -11,13 +27,34 @@ import microsoft.aspnet.signalr.client.MessageReceivedHandler;
 import microsoft.aspnet.signalr.client.hubs.HubConnection;
 import microsoft.aspnet.signalr.client.hubs.HubProxy;
 
-import com.google.gson.JsonElement;
-
-import java.util.Scanner;
-
+import microsoft.aspnet.signalr.client.SignalRFuture;
+import microsoft.aspnet.signalr.client.hubs.HubConnection;
+import microsoft.aspnet.signalr.client.hubs.HubProxy;
+import microsoft.aspnet.signalr.client.hubs.SubscriptionHandler;
+import microsoft.aspnet.signalr.client.hubs.SubscriptionHandler1;
 
 public class ChatActivity extends Activity {
 
+    com.microsoft.signalr.HubConnection connection;
+
+    private class ConnectionAsync extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            String response = null;
+            try {
+                connectionRun();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String response) {}
+
+    }
 
     private void connectionRun() {
 
@@ -29,90 +66,33 @@ public class ChatActivity extends Activity {
                 System.out.println(message);
             }
         };
+        Resources res = getResources();
+        String chatConnectionUrl = res.getString(R.string.chatConnectionUrl);
+        String url1 = chatConnectionUrl + "/hubs/chat";
 
-        // Connect to the server
-        HubConnection conn = new HubConnection("http://signalr-test-server.azurewebsites.net/", "", true, logger);
+        connection = HubConnectionBuilder.create(url1).build();
 
-        // Create the hub proxy
-        HubProxy proxy = conn.createHubProxy("ChatHub");
-
-        proxy.subscribe(new Object() {
-            @SuppressWarnings("unused")
-            public void messageReceived(String name, String message) {
-                System.out.println(name + ": " + message);
-            }
-        });
-
-        // Subscribe to the error event
-        conn.error(new ErrorCallback() {
-
-            @Override
-            public void onError(Throwable error) {
-                error.printStackTrace();
-            }
-        });
-
-        // Subscribe to the connected event
-        conn.connected(new Runnable() {
-
-            @Override
-            public void run() {
-                System.out.println("CONNECTED");
-            }
-        });
-
-        // Subscribe to the closed event
-        conn.closed(new Runnable() {
-
-            @Override
-            public void run() {
-                System.out.println("DISCONNECTED");
-            }
-        });
-
-        // Start the connection
-        conn.start()
-                .done(new Action<Void>() {
-
-                    @Override
-                    public void run(Void obj) throws Exception {
-                        System.out.println("Done Connecting!");
-                    }
-                });
-
-        // Subscribe to the received event
-        conn.received(new MessageReceivedHandler() {
-
-            @Override
-            public void onMessageReceived(JsonElement json) {
-                System.out.println("RAW received message: " + json.toString());
-            }
-        });
-
-        // Read lines and send them as messages.
-        Scanner inputReader = new Scanner(System.in);
-
-        String line = inputReader.nextLine();
-        while (!"exit".equals(line)) {
-            proxy.invoke("send", "Console", line).done(new Action<Void>() {
-
-                @Override
-                public void run(Void obj) throws Exception {
-                    System.out.println("SENT!");
-                }
-            });
-
-            line = inputReader.next();
+        if (connection.getConnectionState() != null) {
+            connection.start().blockingAwait();
         }
 
-        inputReader.close();
+        SessionManager session = new SessionManager(getApplicationContext());
+        HashMap<String, String> userinfo = session.getUserDetails();
 
-        conn.stop();
+        connection.invoke(String.class,"JoinGroup", userinfo.get("chatToken"));
+
+        Toast.makeText(getApplicationContext(), connection.toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    private void off() {
+        connection.stop();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        new ConnectionAsync().execute();
     }
 }
